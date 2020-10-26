962bb5164399e35b8deb3ad5e8dfcf645059c7ac
king398 <mithilsalunkhe@outlook.com>
2020-10-26 12:48:05 +0530

    a

--- main.py
+++ main.py
@@ -1,36 +1,99 @@
 import tensorflow as tf
-import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, MaxPool2D, AveragePooling2D, Dropout, Dense, Flatten, LeakyReLU
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Conv2DTranspose, AvgPool2D
+from tensorflow.keras.layers import LeakyReLU
+import matplotlib.pyplot as plt
+import numpy as np
 
 train = tf.keras.preprocessing.image_dataset_from_directory(
 	r"F:\Pycharm_projects\pneumonia detection with deep learning\-pneumonia-detection-with-deep-learning\chest_xray\train",
 	labels="inferred", image_size=(64, 64)
 	, shuffle=True)
+
 test = tf.keras.preprocessing.image_dataset_from_directory(
 	r"F:\Pycharm_projects\pneumonia detection with deep learning\-pneumonia-detection-with-deep-learning\chest_xray\test",
 	labels="inferred", image_size=(64, 64), shuffle=True)
-model = tf.keras.Sequential()
-model.add(Conv2D(filters=32, kernel_size=2, strides=1, activation="tanh", padding="same"))
-model.add(Conv2D(filters=16, kernel_size=2, strides=1, activation="relu", padding="same"))
-model.add(LeakyReLU())
-model.add(MaxPool2D(pool_size=2, strides=2, padding="same"))
-model.add(Flatten())
-model.add(Dense(256))
-model.add(LeakyReLU())
-model.add(Dense(128))
-model.add(LeakyReLU())
-model.add(Dense(64))
-model.add(LeakyReLU())
-model.add(Dense(32))
-model.add(LeakyReLU())
-model.add(Dense(16))
-model.add(LeakyReLU())
-model.add(Dense(2, activation="softmax"))
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train, validation_data=test, batch_size=2, shuffle=True, epochs=1)
-model.summary()
-plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True, dpi=1200)
-model.evaluate(test)
-print(type(test))
 
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2DTranspose(kernel_size=2, filters=256, strides=1, padding="same"))
+model.add(Conv2D(kernel_size=2, strides=1, filters=128, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=128, padding="same"))
+
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=64, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=64, padding="same"))
+
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=32, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=32, padding="same"))
+
+model.add(AvgPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(kernel_size=2, strides=1, filters=16, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=16, padding="same"))
+
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=8, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=8, padding="same"))
+
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=4, padding="same"))
+
+model.add(Conv2D(kernel_size=2, strides=1, filters=4, padding="same"))
+
+model.add(AvgPool2D(pool_size=2, strides=1, padding="same"))
+
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(Flatten())
+model.add(Dense(512, activation="relu"))
+
+model.add(Dense(256, activation="relu"))
+
+model.add(Dense(128, activation="relu"))
+
+model.add(Dense(64, activation="relu"))
+
+model.add(Dense(32, activation="relu"))
+
+model.add(Dense(16, activation="relu"))
+
+model.add(Dense(8, activation="relu"))
+
+model.add(Dense(2, activation="softmax"))
+
+model.compile(optimizer="adam", metrics=["accuracy"], loss="sparse_categorical_crossentropy")
+checkpoint_filepath = r'F:\Pycharm_projects\pneumonia detection with deep learning\-pneumonia-detection-with-deep-learning\models '
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+	filepath=checkpoint_filepath,
+	save_weights_only=True,
+	monitor='val_accuracy',
+	mode='max',
+	save_best_only=True)
+model.fit(train, validation_data=test, epochs=5, shuffle=True, batch_size=2, callbacks=model_checkpoint_callback)
+model.load_weights(checkpoint_filepath)
+model.summary()
+tf.keras.utils.plot_model(model, to_file="dot_ig_file.png", show_shapes=True, show_layer_names=True, dpi=1200)
+model.evaluate(test)
+model.evaluate(train)
+for x, y in test:
+	print(x, y)
+image_path = r"F:\Pycharm_projects\pneumonia detection with deep learning\-pneumonia-detection-with-deep-learning\images for showing\NORMAL-1283091-0001.jpeg"
+image = tf.keras.preprocessing.image.load_img(image_path, target_size=[64, 64])
+input_arr = tf.keras.preprocessing.image.img_to_array(image)
+input_arr = np.array([input_arr])  # Convert single image to a batch.
+predictions = model.predict(input_arr)
+print(np.argmax(predictions))
