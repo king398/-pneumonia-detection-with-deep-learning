48b152b3cb6049dc0bcdf2170f93db1547d0baad
king398 <mithilsalunkhe@outlook.com>
2020-11-05 10:44:52 +0530

    a

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Made by mithil salunkhe
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Conv2DTranspose, AvgPool2D, DepthwiseConv2D
 import numpy as np
@@ -11,13 +12,17 @@ test = tf.keras.preprocessing.image_dataset_from_directory(
 	r"F:\Pycharm_projects\pneumonia detection with deep learning\-pneumonia-detection-with-deep-learning\chest_xray\test",
 	labels="inferred", image_size=(64, 64), shuffle=True)
 
+
 model = tf.keras.Sequential()
 
 model.add(tf.keras.layers.BatchNormalization())
 model.add(Conv2DTranspose(kernel_size=2, filters=256, strides=1, padding="same"))
+model.add(Conv2D(kernel_size=2, strides=1, filters=256, padding="same"))
+model.add(Conv2D(kernel_size=2, strides=1, filters=256, padding="same"))
+
+
 model.add(Conv2D(kernel_size=2, strides=1, filters=128, padding="same"))
 model.add(Conv2D(kernel_size=2, strides=1, filters=128, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 
 model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 model.add(tf.keras.layers.BatchNormalization())
@@ -25,40 +30,37 @@ model.add(tf.keras.layers.BatchNormalization())
 model.add(Conv2D(kernel_size=2, strides=1, filters=64, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=64, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 
 model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=32, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=32, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 
-model.add(AvgPool2D(pool_size=2, strides=1, padding="same"))
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 
 model.add(tf.keras.layers.BatchNormalization())
 model.add(Conv2D(kernel_size=2, strides=1, filters=16, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=16, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=8, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=8, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=4, padding="same"))
 
 model.add(Conv2D(kernel_size=2, strides=1, filters=4, padding="same"))
-model.add(DepthwiseConv2D(kernel_size=2, strides=1))
 
-model.add(AvgPool2D(pool_size=2, strides=1, padding="same"))
+model.add(MaxPool2D(pool_size=2, strides=1, padding="same"))
 
 model.add(tf.keras.layers.BatchNormalization())
 
 model.add(Flatten())
+model.add(Dense(1028, activation="relu"))
+
 model.add(Dense(512, activation="relu"))
 
 model.add(Dense(256, activation="relu"))
@@ -95,5 +97,8 @@ image_path = r"F:\Pycharm_projects\pneumonia detection with deep learning\-pneum
 image = tf.keras.preprocessing.image.load_img(image_path, target_size=[64, 64])
 input_arr = tf.keras.preprocessing.image.img_to_array(image)
 input_arr = np.array([input_arr])  # Convert single image to a batch.
-predictions = model.predict(input_arr)
-print(np.argmax(predictions))
+predictions = np.argmax(model.predict(input_arr))
+if predictions == 0:
+	print("The Patient is Not Infected With pneumonia")
+elif predictions == 1:
+	print("The Patient is Infected With pneumonia")
